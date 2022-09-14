b7c5c317d7a3cfc58c6c57058ebec7d41c6d58d6
ahuja2666 <ahujaharshit26@gmail.com>
2022-09-14 20:37:41 +0530

    assignment-2-completed

---
+++ readFromLine.js
@@ -0,0 +1,12 @@
+var readline = require('readline');
+
+var rl = readline.createInterface(
+  process.stdin, process.stdout);
+
+
+rl.question('Please enter your name: ', (name) => {
+  console.log('Hello ' + name);
+  rl.close();
+});
+
+
